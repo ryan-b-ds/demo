dea2912eabbadc502975f2f8d2bd4737a000500a
Langa-pdf <anam.sultana0712@gmail.com>
2026-03-29 22:43:40 -0500

    Update SecondaryController.java
    comment changes

--- demo/src/main/java/com/example/SecondaryController.java
+++ demo/src/main/java/com/example/SecondaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
-    // This class is the controller for the secondary view. It contains a method to switch back to the primary view when a button is clicked. The method uses the App class to set the root of the scene to the primary view.
+    // add javadoc comment here - anam 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
