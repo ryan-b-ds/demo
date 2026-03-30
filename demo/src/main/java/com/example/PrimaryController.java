183c33743deac24f44d6d7f335ce3ef7eadb1cbf
att-grey <greyddrr@gmail.com>
2026-03-29 22:39:54 -0500

    Update PrimaryController.java

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
+    //add javadoc comment here - grey
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
